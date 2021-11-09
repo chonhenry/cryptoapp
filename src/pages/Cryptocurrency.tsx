d4e46065481f37314a573679d3bd6f023dbf924e
Henry Chon 2 <chonhenry@outlook.com>
2021-11-08 23:16:48 -0500

    deleted unnecessary code

--- src/pages/Cryptocurrency.tsx
+++ src/pages/Cryptocurrency.tsx
@@ -19,40 +19,19 @@ const Cryptocurrency: React.FC = () => {
     try {
       const data = await getCrypto(id);
       setCrypto(data);
-      console.log(data.name);
+
       const news = await getCryptoNews(data.name);
       setNews(news);
     } catch (error) {}
   };
 
-  // const fetchCryptoNews = async (q: string) => {
-  //   try {
-  //     const news = await getCryptoNews(q);
-  //     setNews(news);
-  //   } catch (error) {}
-  // };
-
   useEffect(() => {
     const getData = async () => {
       await fetchCryptoInfo(parseInt(cryptoId));
-
-      // if (crypto) {
-      //   await fetchCryptoNews(crypto.name);
-      // }
     };
 
     getData();
   }, [cryptoId]);
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     if (crypto) {
-  //       await fetchCryptoNews(crypto.name);
-  //     }
-  //   };
-
-  //   getData();
-  // }, [crypto]);
 
   return (
     <div className="w-full mt-6 m-auto pb-32  md:max-w-5xl">

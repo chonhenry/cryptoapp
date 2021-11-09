39441df16be39a04a43696ede1448af981ff85f1
Henry Chon 2 <chonhenry@outlook.com>
2021-11-08 22:37:49 -0500

    set search result background to white

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const SearchResult: React.FC<Props> = ({ name, symbol, id, onClick }) => {
   return (
-    <div className="dark:bg-white text-sm hover:bg-gray-200" onClick={onClick}>
+    <div className="bg-white text-sm hover:bg-gray-200" onClick={onClick}>
       <Link className="block w-full px-3 py-2" to={`/crypto/${id}`}>
         <span className="inline-block w-16">{symbol}</span>
         <span>{name}</span>

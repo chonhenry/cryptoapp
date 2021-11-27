d6ac304fa9d18d97f7ef1e263702201acb7862d4
Henry Chon 2 <chonhenry@outlook.com>
2021-11-27 17:44:09 -0500

    display news on homepage

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../state/store";
-import { Link } from "react-router-dom";
 import { CryptosStatus } from "../state/slices/cryptosSlice";
+import { getCryptoNews, CryptoNews } from "../API/CryptoNewsApi";
 import CryptoCard from "../components/CryptoCard";
+import News from "../components/News";
 
 const Main: React.FC = () => {
   const cryptos = useSelector((state: RootState) => state.cryptos.list);
   const loading = useSelector((state: RootState) => state.cryptos.status);
 
+  const [news, setNews] = useState<CryptoNews[]>([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    getCryptoNews("cryptocurrency")
+      .then((res) => {
+        setNews(res);
+      })
+      .catch((error: any) => {
+        setError(true);
+      });
+  }, []);
+
   return (
     <div className="max-w-5xl mt-6 m-auto flex justify-center flex-col">
-      <section className="px-3 sm:px-0">
+      <section className="px-3 sm:px-0 mb-10">
         <div className="text-3xl mb-3 dark:text-white">
           Top 10 Cryptocurrencies in the world
         </div>
@@ -31,6 +45,20 @@ const Main: React.FC = () => {
             ))}
           </div>
         )}
+      </section>
+
+      <section className="px-3 sm:px-0 mb-20">
+        <div className="text-3xl mb-3 dark:text-white">
+          Latest Cryptocurrencies news
+        </div>
+        {news.length > 0 && (
+          <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 sm:gap-3">
+            {news.map((n) => (
+              <News news={n} />
+            ))}
+          </div>
+        )}
+        {error && <div className="dark:text-white">News not available</div>}
       </section>
     </div>
   );
